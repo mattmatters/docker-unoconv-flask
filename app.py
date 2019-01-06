7c43127e91cbe50e43e0706ec31a28f795abf4ff
Matt Lewis <domattthings@gmail.com>
2019-01-06 12:26:51 -0800

    Remove flask_restful

--- app.py
+++ app.py
@@ -1,26 +1,34 @@
+"""Document Coverter API"""
 import os
 import subprocess
 import tempfile
 
-from flask import Flask
-from flask import request
+from flask import Flask, request
 from flask.helpers import make_response
-from flask_restful import Resource, Api
-
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
-api = Api(app)
-
+app.config['MAX_CONTENT_LENGTH'] = 60 * 1024 * 1024 # 60mb
 
 class UnoconvConverter(object):
-
     def convert(self, file, input_format, output_format):
         temp_path = tempfile.NamedTemporaryFile(suffix=".%s" % (input_format, ))
         temp_path.write(file)
         temp_path.flush()
 
         unoconv_bin = 'unoconv'
-        command = [unoconv_bin, '--stdout', '-e', 'UseLosslessCompression=false', '-e', 'ReduceImageResolution=false', '--format', output_format, temp_path.name]
+        command = [
+            unoconv_bin,
+            '--stdout',
+            '-e',
+            'UseLosslessCompression=false',
+            '-e',
+            'ReduceImageResolution=false',
+            '--format',
+            output_format,
+            temp_path.name
+        ]
+
         p = subprocess.Popen(command,
                              stdin=subprocess.PIPE,
                              stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -33,19 +41,15 @@ class UnoconvConverter(object):
 
         return data
 
-
-class UnoconvResource(Resource):
-
-    def post(self, output_format):
+@app.route('/convert/<string:output_format>', methods=['POST'])
+def convert(output_format):
         file = request.files['file']
-        extension = os.path.splitext(file.filename)[1][1:]
+        filename = secure_filename(file.filename)
+        extension = os.path.splitext(filename)[1][1:]
         converter = UnoconvConverter()
 
         raw_bytes = converter.convert(file.read(), extension, output_format)
         response = make_response(raw_bytes)
         response.headers['Content-Type'] = "application/octet-stream"
-        response.headers['Content-Disposition'] = "inline; filename=converted.%s" % (output_format, )
+        response.headers['Content-Disposition'] = "inline; filename=" + filename + ".%s" % (output_format, )
         return response
-
-api.add_resource(UnoconvResource, '/unoconv/<string:output_format>/')
-
